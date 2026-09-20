d14ad4d87e207829812104a71f873cb042860f4d
Vladislav Astakhin <105276120+VerTiZt@users.noreply.github.com>
2026-09-20 12:54:07 +0000

    lab2: complete

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int len = strlen(str); // длина строки
+	
+    // идем до середины строки и меняем местами противоположные символы
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i]; // сохраняем текущий левый символ
+        str[i] = str[len - 1 - i]; // на его место ставим симметричный правый
+        str[len - 1 - i] = temp; // на место правого ставим сохраненный левый
+    }
 }
 
